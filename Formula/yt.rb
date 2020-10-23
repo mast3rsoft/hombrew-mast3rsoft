5b05e059a147dc71f38007d7e0e436170359dea7
jakobneufeld <jakob.n.neufeld@gmail.com>
2020-10-23 15:36:09 +0200

    Update yt.rb

--- Formula/yt.rb
+++ Formula/yt.rb
@@ -2,8 +2,8 @@
 class Yt < Formula
   desc "Youtube cli"
   homepage "mast3rsoft.com"
-  url "https://github.com/jakobneufeld/yt.git"
-  head "https://github.com/jakobneufeld/yt.git"
+  url "https://github.com/jakobneufeld/yt.git" , :branch => "main"
+  head "https://github.com/jakobneufeld/yt.git", :branch => "main"
 
   version "1.0.0"
   license "MIT"
@@ -14,13 +14,13 @@ class Yt < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
    
-    # system "cmake", ".", *std_cmake_args
-    system "go", "get" , "github.com/fatih/color"
-    system "go", "build", "-o", "yt"
+    #system "cmake", ".", *std_cmake_args
+   system "go", "get" , "github.com/fatih/color"
+   system "go", "build", "-o", "yt"
     bin.install "yt"
   end
   test do
-    # `test do` will create, run in and delete a temporary directory.
+    ## `test do` will create, run in and delete a temporary directory.
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
